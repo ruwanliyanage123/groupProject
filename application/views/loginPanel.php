1c322f9f667e857f625d70bde3aa88e2a18c465f
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-08 23:10:25 +0530

    add sign up button

--- application/views/loginPanel.php
+++ application/views/loginPanel.php
@@ -14,34 +14,38 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-
+</head>  
 <body>
+    
     <div class="imgcontainer">
             <img src="<?php echo base_url('assets/images/loginMan.png');?>" alt="Avatar" class="avatar" >
             </div>
             <br />  
-            <form action="login" method = "post">
-             <div class="container">          
+            <form action="login" method = "post" >
+             <div class="text-center">          
             <label for='username'>Username</label>
-            <input type='text' name='username' id='username' width = "200" required size='25'placeholder="Enter your username"/>
+            <input type='text' name='username' id='username' required size='25'placeholder=" Enter your username"/>
             <br />
             <label for='password'>Password</label>
-            <input type='password' name='password' id='password' size='25' placeholder="Enter your password" required /><br />   
-            <input type='Submit' value='Login' />   
+            <input type='password' name='password' id='password' size='25' placeholder=" Enter your password" required /><br />   
+            <div class = "container" width = "800px">
+            <input type='Submit' value='Login' class="btn btn-primary btn-md"/>   
             <label>
             <input type="checkbox" checked="checked" name="remember"> Remember me
             </label> 
             </div>
-            <div class="container" style="background-color:#f1f1f1">
-    <button type="button" class="cancelbtn">Cancel</button>
+            </div>
+            <div class="text-center" style="background-color:#f1f1f1">
+    <button type="button" class="signupbtn" onclick="location.href='registerView'">Click here to sign up!</button>
     <span class="psw">Forgot <a href="#">password?</a></span>
   </div>
+
         </form>
-           
+          
 </body>
 </html>
 
  
-
+<!--onClick="this.form.reset()"-->
 
 
