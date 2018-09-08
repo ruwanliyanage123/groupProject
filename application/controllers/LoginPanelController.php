19764e0955dc3eab477bb856f1201f4fd862af9f
dinithiW <36946185+dinithiW@users.noreply.github.com>
2018-09-08 23:07:48 +0530

    add method to load register page

--- application/controllers/LoginPanelController.php
+++ application/controllers/LoginPanelController.php
@@ -5,19 +5,20 @@ class LoginPanelController extends CI_Controller{
 	public function __construct() {
 		parent::__construct();
 		$this->load->helper('url');
-		$this->load->helper('html');
-
     }
+    
 	public function index(){
 		$this->load->view('loginPanel');
-
 	}
-
 
     public function process(){
         //echo 'hahahahahha';
         $this->load->model('loginPanelValidation');
         $this->loginPanelValidation->validate();  
+    }
+
+    public function register(){
+    	$this->load->view('registerPanel');
     }
 
 }
